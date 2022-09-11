828c0008752428b7e358b6b425287a315cf8c04d
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-09-11 17:56:56 +0200

    code challenge 2 section2

--- JavaScript Fundamentals – Part 2/code_challenge2/main.js
+++ JavaScript Fundamentals – Part 2/code_challenge2/main.js
@@ -1,2 +1,16 @@
-//! ====================Code Challenge 1==================
+//! ====================Code Challenge 2==================
 
+let bills = [125, 555, 44];
+let tips = [];
+let total = [];
+const calcTips = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTips(bills[i]));
+  total.push(bills[i] + tips[i]);
+  console.log(
+    `Bill Value ${bills[i]} Tip Value ${tips[i]} \n total = ${total[i]}`
+  );
+}
