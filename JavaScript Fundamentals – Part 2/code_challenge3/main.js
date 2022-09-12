96a57250b3462dabce8fc003f0c409a112e98c1a
abdallahhesham99 <abdallahhesham414@gmail.com>
2022-09-12 15:07:09 +0200

    code challenge3 section 2

--- JavaScript Fundamentals – Part 2/code_challenge3/main.js
+++ JavaScript Fundamentals – Part 2/code_challenge3/main.js
@@ -1,2 +1,33 @@
-//! ====================Code Challenge 1==================
+//! ====================Code Challenge 3==================
 
+const markObj = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    return this.mass / this.height ** 2;
+  },
+};
+
+const johnObj = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    return this.mass / this.height ** 2;
+  },
+};
+
+if (markObj.calcBMI() > johnObj.calcBMI()) {
+  console.log(
+    `${markObj.fullName}'s BMI ${markObj.calcBMI().toFixed(2)} is higher than ${
+      johnObj.fullName
+    }'s BMI ${johnObj.calcBMI().toFixed(2)}`
+  );
+} else {
+  console.log(
+    `${johnObj.fullName}'s BMI ${johnObj.calcBMI().toFixed(2)} is higher than ${
+      markObj.fullName
+    }'s BMI ${markObj.calcBMI().toFixed(2)}`
+  );
+}
